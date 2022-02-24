668b40bcb7fe3c9ddeba8dc64717d23f3937987c
theo labrosse <theo.labrosse@etu.univ-lyon1.fr>
2022-02-24 12:14:24 +0100

    error to correct

--- app/src/main/java/fr/iutlyon1/theo/springproject/UserForm.kt
+++ app/src/main/java/fr/iutlyon1/theo/springproject/UserForm.kt
@@ -1,11 +1,4 @@
 package fr.iutlyon1.theo.springproject
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class UserForm : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_form)
-    }
+class UserForm {
 }
